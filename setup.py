86f20f5e19597714a6992a8e52159066d3291c84
kwanern <kwanern@umich.edu>
2019-05-30 15:19:12 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,11 @@ with open("README", 'r') as f:
 
 setup(
    name='fkwan',
-   version='1.0',
+   version='1.00',
    description='Package with functions',
    license="MIT",
    long_description=long_description,
    author='Fu Ern Kwan',
    author_email='kwanern@umich.edu',
-   url="http://www.foopackage.com/",
    packages=find_packages(),  #same as name
-   install_requires=[
-       'spark',
-       'pyspark',
-       'sklea'
-   ], #external packages as dependencies
 )
