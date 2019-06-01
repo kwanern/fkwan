35d83d09ef82fc9d36742d39c043fec67ad6b56b
kwanern <kwanern@umich.edu>
2019-05-31 17:59:36 -0700

    new udf

---
+++ pyspark/udf.py
@@ -0,0 +1,8 @@
+@sqlf.udf("string")
+def concat_string_arrays(*ls):
+    """
+        This function concat multiple string columns into one column with separator &
+        :param ls: array of column names
+        :return: string column
+    """
+    return ' & '.join(filter(None, ls))
