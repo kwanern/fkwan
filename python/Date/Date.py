73814026a63525111f6c0746b77c94cd50b3d385
kwanern <kwanern@umich.edu>
2019-08-12 16:08:46 -0700

    [1.3.1] - Python Date

--- python/Date/Date.py
+++ python/Date/Date.py
@@ -3,7 +3,10 @@ from ...libraries import *
 
 class Date(object):
     def __init__(self, dt):
-        self.date = pd.to_datetime(pd.Timestamp(dt))
+        self.date = pd.to_datetime(pd.Timestamp(dt)).date()
+
+    def __repr__(self):
+        return str(self.date)
 
     # adding days to date
     def __add__(self, x):
@@ -18,4 +21,20 @@ class Date(object):
             return self.date + pd.Timedelta(-x, unit='d')
         else:
             return self.date - x.date
+
+
+def date_range(start_date, end_date, format='%Y%m%d', freq='D'):
+    """
+        This function creates an array of date integer, by 1 day
+
+        :param start_date: string
+        :param end_date: string
+        :param format: string
+        :param freq: string
+        :return: array of date
+
+        Examples:
+        >>> date_range('20190101', '20190201', format='%Y%m%d', freq='2D')
+    """
+    return [str(d.strftime(format)) for d in pd.date_range(str(start_date.date), str(end_date.date), freq=freq)]
 
