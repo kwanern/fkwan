9bae67a1b81a3e4061c996ee779bd0b7fa80878f
kwanern <kwanern@umich.edu>
2019-08-12 14:55:53 -0700

    [1.2.3] - Python time - date_range

--- python/time.py
+++ python/time.py
@@ -15,18 +15,20 @@ def days(day):
 
 
 # Read Table Function
-def date_range(start_date, end_date):
+def date_range(start_date, end_date, format='%Y%m%d', freq='D'):
     """
         This function creates an array of date integer, by 1 day
 
         :param start_date: string
         :param end_date: string
-        :return: array of date integer
+        :param format: string
+        :param freq: string
+        :return: array of date
 
         Examples:
-        >>> date_range('20190101', '20190201')
+        >>> date_range('20190101', '20190201', format='%Y%m%d', freq='2D')
     """
-    return [int(d.strftime('%Y%m%d')) for d in pd.date_range(start_date, end_date)]
+    return [int(d.strftime(format)) for d in pd.date_range(start_date, end_date, freq=freq)]
 
 
 # Convert time delta to days_hours_minutes
