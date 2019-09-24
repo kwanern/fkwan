9ad846037c3e8fb94138c82758f5b5b61c34a67a
kwanern <kwanern@umich.edu>
2019-09-24 09:45:04 -0700

    [1.3.5] CustomerSegmentation - Reformat code, add food segmentation

--- pyspark/CustomerProfiler/CustomerSegmentation.py
+++ pyspark/CustomerProfiler/CustomerSegmentation.py
@@ -35,6 +35,7 @@ class Segmentation(object):
         self.name = None
         self.period = period
         self.seg_date = date
+        self.type = None
 
         self.pos = (
             self.spark
@@ -59,7 +60,7 @@ class Segmentation(object):
             )
         )
 
-    def beverage_segmentation(self, product, cohort=None, base=False, title=None):
+    def __segmentation(self, tp, product, cohort=None, base=False, title=None):
         self.start_date = product["Promo_Start_Date"]
         self.end_date = product["Promo_End_Date"]
         self.products_names = product["Product_Name"]
@@ -67,6 +68,7 @@ class Segmentation(object):
         self.products_id = product["Id"]
         self.pch_frq_min = product["Purchased_Freq_Min"]
         self.pch_frq_max = product["Purchased_Freq_Max"]
+        self.type = tp
 
         if title:
             self.name = title
@@ -123,10 +125,10 @@ class Segmentation(object):
                 sqlf.lit(self.name)
             )
             .withColumn(
-                "beverage_primary_segment",
-                sqlf.coalesce(sqlf.col("sr.bev_primary_segment"),sqlf.col("nonsr.bev_primary_segment"))
+                self.type,
+                sqlf.coalesce(sqlf.col("sr."+self.type),sqlf.col("nonsr."+self.type))
             )
-            .groupBy("Product", "Customer_Type", "beverage_primary_segment")
+            .groupBy("Product", "Customer_Type", self.type)
             .agg(
                 (sqlf.sum(sqlf.col("GrossLineItemQty")) / sqlf.countDistinct(sqlf.col("Id"))).alias(
                     "units_cust"),
@@ -159,10 +161,10 @@ class Segmentation(object):
                     sqlf.lit("Baseline")
                 )
                 .withColumn(
-                    "beverage_primary_segment",
-                    sqlf.coalesce(sqlf.col("sr.bev_primary_segment"), sqlf.col("nonsr.bev_primary_segment"))
+                    self.type,
+                    sqlf.coalesce(sqlf.col("sr."+self.type), sqlf.col("nonsr."+self.type))
                 )
-                .groupBy("Product", "Customer_Type", "beverage_primary_segment")
+                .groupBy("Product", "Customer_Type", self.type)
                 .agg(
                     (sqlf.sum(sqlf.col("GrossLineItemQty")) / sqlf.countDistinct(sqlf.col("Id"))).alias(
                         "units_cust"),
@@ -190,135 +192,11 @@ class Segmentation(object):
 
         return result
 
-    def flavor_segmentation(self, product, cohort=None, base=False, title=None):
-        self.start_date = product["Promo_Start_Date"]
-        self.end_date = product["Promo_End_Date"]
-        self.products_names = product["Product_Name"]
-        self.level = product["EPH_level"]
-        self.products_id = product["Id"]
-        self.pch_frq_min = product["Purchased_Freq_Min"]
-        self.pch_frq_max = product["Purchased_Freq_Max"]
+    def beverage_segmentation(self, product, tp="bev_primary_segment", cohort=None, base=False, title=None):
+        return self.__segmentation(self, product, tp=tp, cohort=cohort, base=base, title=title)
 
-        if title:
-            self.name = title
-        else:
-            self.name = self.products_names
+    def flavor_segmentation(self, product, tp="flavor_primary_segment", cohort=None, base=False, title=None):
+        return self.__segmentation(self, product, tp=tp, cohort=cohort, base=base, title=title)
 
-        self.pos = (
-            self.pos
-            .alias("pos")
-            .filter(sqlf.col("BusinessDate").between(self.start_date, self.end_date))
-            .join(
-                self.spark
-                    .table("ttran.taste_segments_v3")
-                    .alias("sr"),
-                ((sqlf.col("pos.Id") == sqlf.col("sr.GuidId")) &
-                 (sqlf.col("sr.period") == self.period) &
-                 (sqlf.col("sr.end_date") == self.seg_date)),
-                how="left"
-            )
-            .join(
-                self.spark
-                    .table("ttran.taste_segments_non_sr_v3")
-                    .alias("nonsr"),
-                ((sqlf.col("pos.Id") == sqlf.col("nonsr.FirstPaymentToken")) &
-                 (sqlf.col("nonsr.period") == self.period) &
-                 (sqlf.col("nonsr.end_date") == self.seg_date)),
-                how="left"
-            )
-            .withColumn(
-                "Product",
-                sqlf.lit(self.name)
-            )
-            .where(
-                sqlf.col("sr.GuidId").isNotNull() |
-                sqlf.col("nonsr.FirstPaymentToken").isNotNull()
-            )
-        )
-
-        if cohort:
-            self.pos = (
-                self.pos.alias("result")
-                    .join(
-                    cohort.pf_spdf.alias("cohort"),
-                    sqlf.col("result.Id") == sqlf.col("cohort.Id"),
-                    how="inner"
-                )
-            )
-
-        result = (
-            self.pos
-            .filter(sqlf.col(self.level).isin(self.products_id))
-            .withColumn(
-                "Product",
-                sqlf.lit(self.name)
-            )
-            .withColumn(
-                "flavor_primary_segment",
-                sqlf.coalesce(sqlf.col("sr.flavor_primary_segment"), sqlf.col("nonsr.flavor_primary_segment"))
-            )
-            .groupBy("Product", "Customer_Type", "flavor_primary_segment")
-            .agg(
-                (sqlf.sum(sqlf.col("GrossLineItemQty")) / sqlf.countDistinct(sqlf.col("Id"))).alias(
-                    "units_cust"),
-                (sqlf.sum(sqlf.col("GrossLineItemQty"))).alias("units"),
-                (sqlf.sum(sqlf.col("NetDiscountedSalesAmount"))).alias("NDS")
-            )
-        )
-
-        result = (
-            result
-            .withColumn(
-                'units_cust_proportion',
-                sqlf.col('units_cust') / sqlf.sum('units_cust').over(Window.partitionBy("Customer_Type"))
-            )
-            .withColumn(
-                'total_units_proportion',
-                sqlf.col('units') / sqlf.sum('units').over(Window.partitionBy("Customer_Type"))
-            )
-            .withColumn(
-                'total_nds_proportion',
-                sqlf.col('NDS') / sqlf.sum('NDS').over(Window.partitionBy("Customer_Type"))
-            )
-        )
-
-        if base:
-            base = (
-                self.pos
-                .withColumn(
-                    "Product",
-                    sqlf.lit("Baseline")
-                )
-                .withColumn(
-                    "flavor_primary_segment",
-                    sqlf.coalesce(sqlf.col("sr.flavor_primary_segment"), sqlf.col("nonsr.flavor_primary_segment"))
-                )
-                .groupBy("Product", "Customer_Type", "flavor_primary_segment")
-                .agg(
-                    (sqlf.sum(sqlf.col("GrossLineItemQty")) / sqlf.countDistinct(sqlf.col("Id"))).alias(
-                        "units_cust"),
-                    (sqlf.sum(sqlf.col("GrossLineItemQty"))).alias("units"),
-                    (sqlf.sum(sqlf.col("NetDiscountedSalesAmount"))).alias("NDS")
-                )
-            )
-
-            base = (
-                base
-                .withColumn(
-                    'units_cust_proportion',
-                    sqlf.col('units_cust') / sqlf.sum('units_cust').over(Window.partitionBy("Customer_Type"))
-                )
-                .withColumn(
-                    'total_units_proportion',
-                    sqlf.col('units') / sqlf.sum('units').over(Window.partitionBy("Customer_Type"))
-                )
-                .withColumn(
-                    'total_nds_proportion',
-                    sqlf.col('NDS') / sqlf.sum('NDS').over(Window.partitionBy("Customer_Type"))
-                )
-            )
-            return result.union(base)
-
-        return result
-
-
+    def food_segmentation(self, product, tp="food_primary_segment", cohort=None, base=False, title=None):
+        return self.__segmentation(self, product, tp=tp, cohort=cohort, base=base, title=title)
