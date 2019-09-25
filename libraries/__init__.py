f9c6cb38b9cb1f9fb45bc46a114760bac46473c2
kwanern <kwanern@umich.edu>
2019-09-25 11:38:24 -0700

    [1.3.5] CustomerSegmentation - Reformat code, add food segmentation

--- libraries/__init__.py
+++ libraries/__init__.py
@@ -3,6 +3,8 @@ from pyspark.ml.feature import VectorAssembler
 from pyspark.sql import functions as sqlf
 from pyspark.sql.types import *
 from pyspark.sql import Window
+from functools import reduce
+from pyspark.sql import DataFrame
 
 import numpy as np
 import matplotlib as mat
