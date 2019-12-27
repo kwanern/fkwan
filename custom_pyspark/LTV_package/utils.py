3ff8b8ffc5810f20d8df916409b4b506b044f096
kwanern <kwanern@umich.edu>
2019-12-27 12:41:30 -0800

    update

--- custom_pyspark/LTV_package/utils.py
+++ custom_pyspark/LTV_package/utils.py
@@ -116,7 +116,9 @@ class ltv_validation(ltv):
 
     def collect(self, groupByName="AVG_MONETARY_PERCENTILE"):
         result = (
-            self.validation.groupBy("result." + groupByName)
+            self.validation.groupBy(
+                "AVG_MONETARY_PERCENTILE" if (groupByName=="AVG_MONETARY_PERCENTILE") else "result.{0}".format(groupByName)
+            )
             .agg(
                 sqlf.avg(sqlf.col("result.PRED_CLV")).alias("AVG_PRED_CLV"),
                 sqlf.avg(sqlf.col("result.PRED_VISITS")).alias("AVG_PRED_VISITS"),
