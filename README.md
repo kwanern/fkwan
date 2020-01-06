b835895b95263d6a3d81fc51c7db3a75244e8465
kwanern <kwanern@umich.edu>
2020-01-06 13:08:04 -0800

    [Version 1.4.1] udf - Add describe spark dataframe

--- README.md
+++ README.md
@@ -34,6 +34,7 @@ Customer Profiler Packages:
 * [Version 1.3.5] Customer Segmentation - Reformat code, add food segmentation
 * [Version 1.3.6] Cohort Metric - Engagement takes customer class
 * [Version 1.4.0] LTV - Validation Metrics and Data Pull
+* [Version 1.4.1] udf - Add describe spark dataframe
 
 ## Authors
 
--- custom_pyspark/udf.py
+++ custom_pyspark/udf.py
@@ -3,6 +3,8 @@ from pyspark.sql.functions import pandas_udf, PandasUDFType, udf
 from pyspark.sql.types import StringType
 from functools import reduce
 from pyspark.sql import DataFrame
+import numpy as np
+import pandas as pd
 
 
 def union_all(*dfs):
@@ -31,3 +33,29 @@ def concat(*ls):
     return ' & '.join(filter(None, ls))
 
 concat_string_arrays = udf(concat, StringType())
+
+
+def describe_pd(df_in, columns, deciles=False):
+    """
+        This function return summary statistics of a spark dataframe
+
+        :param df_in: spark dataframe
+        :param columns: array of strings
+        :return: pandas
+
+        Examples:
+        >>> concat_string_arrays(*[re.sub("\s", "_", i) for i in product_names])
+    """
+    if deciles:
+        percentiles = np.array(range(0, 110, 10))
+    else:
+        percentiles = [25, 50, 75]
+
+    percs = np.transpose([np.percentile(df_in.select(x).collect(), percentiles) for x in columns])
+    percs = pd.DataFrame(percs, columns=columns)
+    percs['summary'] = [str(p) + '%' for p in percentiles]
+
+    spark_describe = df_in.describe().toPandas()
+    new_df = pd.concat([spark_describe, percs],ignore_index=True)
+    new_df = new_df.round(2)
+    return new_df[['summary'] + columns]
